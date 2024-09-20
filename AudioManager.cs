8bd61f9980d86ba7a3317a78c29718cfedbddd61
Adem Can Kocaman <85249208+AdemCan61@users.noreply.github.com>
2024-09-20 10:35:17 +0200

    Add files via upload

---
+++ AudioManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("---------- Audio Source ----------")]
+    [SerializeField] AudioSource musicSource; 
+    [SerializeField] AudioSource SFXSource; 
+
+    [Header("---------- Audio Clip ----------")]
+    public AudioClip Aaaaaaah;
+    public AudioClip BrancherCables;
+    public AudioClip Cafe;
+    public AudioClip Dialogue;
+    public AudioClip Discuter;
+    public AudioClip FemmeNe;
+    public AudioClip FemmePos;
+    public AudioClip HommeNe;
+    public AudioClip HommePos;
+    public AudioClip MusiqueBackground;
+    public AudioClip PetiteBalade;
+    public AudioClip ProjetDeGroup;
+    public AudioClip Question1;
+    public AudioClip Question2;
+    public AudioClip Question3;
+    public AudioClip Question4;
+    public AudioClip Question5;
+}
